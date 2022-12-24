35522646080c039bfe5042987a064c8c0f2329ba
FusionSid <siddheshadsv@icloud.com>
2022-12-24 16:11:42 +1300

    feat: Update version and add console script for cli tool

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.2.1"
+VERSION = "1.0.0"
 DESCRIPTION = "Base 69 encoder and decoder"
 
 setup(
@@ -13,6 +13,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    entry_points={"console_scripts": ["base69=base69.__main__:main"]},
 )
 
 # python3 setup.py bdist_wheel
